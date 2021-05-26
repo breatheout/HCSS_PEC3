a1ced918d1f1588f4ab9c91f15a88f6696af8bb1
breatheout <61621673+breatheout@users.noreply.github.com>
2021-05-26 19:03:01 +0100

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.html"],
+  darkMode: false,
+  theme: {
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        nav: "rgba(0, 0, 0, 0.6)",
+        navhov: "rgba(255, 255, 255, 0.2)",
+        header: "rgba(0, 0, 0, 0.8)",
+        white09: "rgba(255, 255, 255, 0.9)",
+        blackopacity3: "rgba(0, 0, 0, 0.6)",
+      }),
+      backgroundImage: {
+        "bw-pattern": "url('../images/background.png')",
+        "merch-sample": "url('../images/pexels-cottonbro-4066293-RESIZE.jpg')",
+      },
+    },
+    fontFamily: {
+      sans: ["Arial", "Helvetica", "sans-serif"],
+      header: ["Anton", "sans-serif"],
+    },
+    flex: {
+      navresp: "1 1 50%",
+    },
+    flexGrow: {
+      2: 2,
+      3: 3,
+    },
+  },
+  variants: {},
+  plugins: [],
+};
